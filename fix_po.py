428cfd12ed954cef572314566e2e7a0e883ed401
Leonardo Pimenta <leoneivaw@gmail.com>
2026-06-26 17:06:35 -0300

    Remove temporary python scripts and add to gitignore

--- fix_po.py
+++
@@ -1,8 +0,0 @@
-import glob
-for file in glob.glob("translate/*.po*"):
-    with open(file, "r") as f:
-        content = f.read()
-    content = content.replace('"Last-Translator:\n"', '"Last-Translator: \\n"\n')
-    content = content.replace('"Language-Team:\n"', '"Language-Team: \\n"\n')
-    with open(file, "w") as f:
-        f.write(content)
